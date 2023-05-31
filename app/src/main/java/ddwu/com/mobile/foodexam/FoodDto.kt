275760dbbb2da567d3315b11ab4297e9fab89d51
aaasa8 <113534445+aaasa8@users.noreply.github.com>
2023-05-31 13:45:36 +0900

    Update FoodDto.kt

--- app/src/main/java/ddwu/com/mobile/foodexam/FoodDto.kt
+++ app/src/main/java/ddwu/com/mobile/foodexam/FoodDto.kt
@@ -1,6 +1,6 @@
 package ddwu.com.mobile.foodexam
 
-data class FoodDto(var food: String, var country: String) {
+data class FoodDto(var food: String, var country: String) : java.to.Serializab {
     override fun toString() = "$food ($country)"
 }
 
